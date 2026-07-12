e9982e3c2c143838eaaf243ef17c5737a5d68dc7
DevAyushKumar <blaze.main.id@gmail.com>
2026-07-12 19:12:41 +0530

    Class object

---
+++ Objects/Class.js
@@ -0,0 +1,12 @@
+class student {
+    constructor (name,age){
+        this.name = name;
+        this.age = age;
+    }
+};
+const s1 = new student("Ayush", 18);
+
+console.log(s1.name);
+console.log(s1["name"]);
+console.log(s1.age);
+console.log(s1["age"]);
