f61b9ca76cb93322c1f823714cd5497ffd201deb
DevAyushKumar <blaze.main.id@gmail.com>
2026-07-13 00:59:40 +0530

    Types of arrays

---
+++ Arrays/TypesOfArrays.js
@@ -0,0 +1,12 @@
+//Array literal 
+const fruits = ["Apple", "Banana", "mango"];
+
+//Array constructor
+const arr = new Array(5,10);
+
+//Arrya.of
+function createarray(...args){
+    return Array.of(...args);
+}
+
+//
