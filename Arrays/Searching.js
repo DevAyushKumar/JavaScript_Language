7d044e040d21d9b682292ee239b1f18a22776bbc
DevAyushKumar <blaze.main.id@gmail.com>
2026-07-14 01:19:14 +0530

    Added rest of the searching meathods

--- Arrays/Searching.js
+++ Arrays/Searching.js
@@ -1,3 +1,16 @@
+//indexOf and lastIndexOf
 const index = ['js', 'css', 'html', 'js'];
 console.log(index.indexOf('js'));
 console.log(index.lastIndexOf('js'));
+
+//includes
+console.log(index.includes('css'));
+
+//find and findindex
+const score = [29, 35, 68, 89, 90, 100];
+const targetScore = score.find(score => score > 80);
+console.log(targetScore);
+
+//Some and every
+console.log(score.some(score => score > 80));
+console.log(score.every(score => score > 0));
