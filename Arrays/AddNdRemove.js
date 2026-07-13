03a365489152c2b4d0866f6188b7c30afeb92f1d
DevAyushKumar <blaze.main.id@gmail.com>
2026-07-13 16:21:44 +0530

    Adding and removing elements in js

---
+++ Arrays/AddNdRemove.js
@@ -0,0 +1,17 @@
+//Adding and removing elements
+//push
+let arr = ["user1", "user2"];
+let queue = arr.push("user3");
+console.log(arr);
+
+//pop
+let Remove = arr.pop();
+console.log(arr);
+
+//unshift
+arr.unshift("person 4");
+console.log(arr);
+
+//shift
+arr.shift();
+console.log(arr);
