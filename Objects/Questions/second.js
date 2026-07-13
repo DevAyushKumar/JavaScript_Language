b37da942bc259c903848e399528361d135a8860a
DevAyushKumar <blaze.main.id@gmail.com>
2026-07-13 21:58:51 +0530

    Second question

---
+++ Objects/Questions/second.js
@@ -0,0 +1,9 @@
+//Write a code to count the number of keys 
+let Obj = {
+    name : "Ayush Kumar",
+    age : 18,
+    role : "Student"
+};
+
+const len = Object.keys(Obj).length;
+console.log(len);
