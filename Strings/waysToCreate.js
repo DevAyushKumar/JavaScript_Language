66712817d40d42fb26f9077b67c66ae51c968a18
DevAyushKumar <blaze.main.id@gmail.com>
2026-07-15 01:00:40 +0530

    Ways to create string

---
+++ Strings/waysToCreate.js
@@ -0,0 +1,25 @@
+//1
+const a = 'string';
+
+//2
+const b = "string";
+
+//3
+const c = `string`;
+
+//4
+let d = String(123);
+let e = String(true);
+let f = String(null);
+
+//5
+let g = new String("hello");
+
+//6
+const h = "he" + "ll" + "o";
+
+//7
+const i = ["h", "e", "l", "l", "o"].join("");
+
+//8
+let j = String.fromCharCode(72, 101, 108, 108, 111);
