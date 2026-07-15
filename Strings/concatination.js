97a130f29473fbdd22363baae498b501ebd889fe
DevAyushKumar <blaze.main.id@gmail.com>
2026-07-15 12:05:51 +0530

    Concationation meathods

---
+++ Strings/concatination.js
@@ -0,0 +1,15 @@
+//operator
+"Hello" + " " + "World";
+
+//+= operator
+let s = "hello";
+s += "World";
+
+//concat() meathod
+"Hello".concat(" ","World");
+
+//Template literals
+`${"Hello"} ${"World"}`;
+
+//.join()
+["Hello","World"].join(" ");
