9e89a2163a779fc6dad809a5744602219367669f
DevAyushKumar <blaze.main.id@gmail.com>
2026-07-15 12:23:52 +0530

    String conversion

---
+++ Strings/stringConversion.js
@@ -0,0 +1,15 @@
+//to string
+String(123);
+String(true);
+String(null);
+String(undefined);
+String([1,2,3]);
+(123).toString();
+123 + "";
+
+//From string
+Number("123");
+Number("123abc");
+parseInt("123abc");
+parseFloat("123abc");
++"123";
